44a826b1c400ffd5909cb3781f48af0ff5f11bf8
Zulfa Juniadi <zulfajuniadi@gmail.com>
2013-12-13 15:01:50 +0800

    intance based examples

---
+++ examples/app/content/tasks/new.js
@@ -0,0 +1,19 @@
+mjs.Define({
+	events: {
+		'keyup #newTask': function(e) {
+			if (e.keyCode === 27) {
+				this.value = '';
+			} else if (e.keyCode === 13) {
+				var value = this.value;
+				this.value = '';
+				tasks.post({
+					title: value
+				}, function(err, response) {
+					if (err === null) {
+						MjsContentInstance.SetDirty('tasks');
+					}
+				});
+			}
+		}
+	}
+});
---
+++ examples/app/content/tasks/tasks.js
@@ -0,0 +1,37 @@
+mjs.Define({
+	handle: '/tasks',
+	events: {
+		'click .remove': function(e) {
+			if (typeof e.target.dataset.id !== 'undefined') {
+				var id = e.target.dataset.id;
+				var rev = e.target.dataset.rev;
+				tasks.remove({
+					_id: id,
+					_rev: rev
+				}, function(err, response) {
+					if (err === null)
+						MjsContentInstance.SetDirty('tasks');
+				})
+			}
+		}
+	},
+	data: function(done) {
+		if (tasks) {
+			tasks.allDocs({
+				include_docs: true
+			}, function(err, data) {
+				if (err) {
+					console.error(err);
+				}
+				if (data && data.rows) {
+					return done(data.rows);
+				}
+				done([]);
+			})
+		}
+	},
+	rendered: function() {
+		MjsContentInstance.SetRendered('tasks/new');
+		document.getElementById('newTask').focus();
+	}
+});
